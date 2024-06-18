f84c03e0aa04c141666d584b6f9777aac1460bd4
erma-T <ermy143@gmail.com>
2024-06-18 11:20:33 +0300

    Final commit for result form

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -87,6 +87,46 @@ export function Card() {
 
       </div>
 
+      <div className="col-span-1 lg:col-span-1 h-full min-h-[20rem]">
+                
+        <div className="flex flex-wrap justify-center justify-items-center">
+          <WobbleCard
+            containerClassName="col-span-1 lg:col-span-1 h-full"
+            className="grid grid-rows-6"
+          >
+            <div  className="row-span-3 w-full ">
+              <div className="w-[18rem] h-[8.5rem] bg-cover bg-center bg-no-repeat bg-[url('/screenshots/ch3-desktop-preview.jpg')]">
+                
+              </div>
+            </div>
+            <div  className="row-span-1 w-full pl-[1rem] py-[0.2rem] justify-center">
+              <h2 className="text-left font-semibold text-white">
+                Result form
+              </h2>
+
+            </div>
+            <div  className="row-span-2 w-full px-[1rem]">
+              <p className="text-left text-sm font-light text-white">
+               A challenge is to build out a result display form.
+              </p>
+              <span className="text-left text-xs italic font-light mx-1 text-white">#HTML</span>
+              <span className="text-left text-xs italic font-light mx-1 text-white">#CSS</span>
+
+            </div>
+            
+            
+          </WobbleCard>
+          <div  className="z-30 flex flex-wrap px-[1rem] py-[0.7rem] w-full justify-center justify-items-stretch">
+              <p className="text-left text-sm font-light text-white">
+                <a href="https://erma-t.github.io/results-summary-component-main-frontendmentor/" target="_blank">Live </a> &nbsp;&nbsp;| &nbsp;&nbsp;
+                <a href="https://github.com/Erma-T/results-summary-component-main-frontendmentor" target="_blank">Git Repo</a> &nbsp;&nbsp;| &nbsp;&nbsp; 
+                <a href="https://www.frontendmentor.io/challenges/contact-form--G-hYlqKJj" target="_blank">Challenge</a></p>
+          </div>
+        
+        </div>
+
+      </div>
+
     </div>
   );
 }
