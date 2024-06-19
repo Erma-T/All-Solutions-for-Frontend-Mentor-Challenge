1c758577e84a47198c88ff3f92a7b722697c8f28
erma-T <ermy143@gmail.com>
2024-06-19 18:11:01 +0300

    Footer link added

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
         
         <footer className="w-full bg-gradient-to-r from-[#140c20] via-[#3c175a] to-[#110632] text-white p-5 px-4 mt-[2rem] text-center">
             <small className="block text-sm font-normal">
-                &copy; 2024 Ermiyas.com | All right reserved.
+                &copy; 2024 <a href="https://ermiyas.com">Ermiyas.com</a> | All right reserved.
             </small>
             
         </footer>
